03843d1b1558ae9bf6f9392b1ab40307d8471aa3
ChrisCluney <ccluney@gmail.com>
2022-01-21 10:58:58 -0600

    Addition implementation working

--- sldc.js
+++ sldc.js
@@ -5,12 +5,14 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
+
+
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +21,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "Analysis Of Requirements is considered part of planning to determine what the application is supposed to do and its requirements. "
+const design = "In this design phase of work, the team makes software design decisions regarding the architecture and make of the software solution. This can involve creating design documents, coding guidelines, and discussing the tools, practices, runtimes, or frameworks that will help the team meet the software requirement specification and goals defined in the requirements gathering phase. "
+const implementation = "In this stage of work, teams build the software solutions based on the design decisions made. Here, teams meet the goals and outcomes set during the software requirements gathering phase by implementing the solution."
+const testingAndIntegration = "In this phase of work, a software implementation is packaged and tested to assure quality. Testing or quality assurance ensures the solutions implemented pass the standard for quality and performance. This can involve unit testing, performing integration and end-to-end tests, verification/validation, and reporting or identifying bugs or defects in the software solution."
+const maintain = "In this stage of work, the software is operationalized to ensure there are no issues or incidents related to the deployment. This stage of work can involve reviewing, understanding, and monitoring network settings, infrastructure configurations, and performance of application services in production. This process can involve incident resolution or management in the course of any issues or changes made to impact a customer or user base. "
